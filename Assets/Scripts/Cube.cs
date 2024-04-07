9a4efda2425d77849bc8028328ccb3ab0caf6e6b
Mateus Oliveira <devmateusalves@gmail.com>
2024-04-07 14:26:08 -0300

    Cube script renamed

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cubes : MonoBehaviour
-{
+public class Cube : MonoBehaviour {
     private Rigidbody rb;
-    private int cablePoint = 0;
+    private bool isGrounded;
+    [SerializeField] private int cablePoint;
     [SerializeField] private float speed = 2f;
     [SerializeField] private GameObject[] cablePoints = new GameObject[12];
+    [SerializeField] private GameObject[] bottomPoints = new GameObject[12];
 
-    void Start()
-    {
+    void Start() {
+        isGrounded = false;
         rb = GetComponent<Rigidbody>();
-        cablePoint = Random.Range(0, 12);
+        //cablePoint = Random.Range(0, 12);
         Debug.Log(cablePoint);
     }
 
-    void Update()
-    {
+    void Update() {
         if (!rb.useGravity) {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
+
+        if (isGrounded) {
+            rb.isKinematic = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
     }
 
-    void OnTriggerEnter(Collider other){
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("BottomPoint")) {
+            isGrounded = true;
+        }
+
         if (other.gameObject == cablePoints[cablePoint]) {
             rb.useGravity = true;
             transform.position = other.gameObject.transform.position;
